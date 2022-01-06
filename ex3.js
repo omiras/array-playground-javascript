fe2fea140c2054fdc634abb6c9b5471d61c1c866
Oscar Miras <oscar.miras@gmail.com>
2022-01-06 15:41:59 +0100

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -3,17 +3,19 @@
  * 
  * Una de las cosas que más cuesta entender al comenzar programación, es que la primera posición del array, viene dada en realidad por la posición "cero".
  * 
- * A esta posición también se le llama "índice"
+ * A la posición que ocupa un elemento en un array se le llama "índice"
  * 
  * Así, lista_peces[0] nos da el primer pez
  * lista_peces[1] nos da el segundo pez
  * ..y así hasta el último.
  * 
- * La última posición de un array siempre es "el número de elementos menos uno"
+ * En el array: let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
+ * 
+ * El array tiene 4 elementos. Para acceder a "Dory" debo poner muchos_peces[0]. Para acceder a "Estrella" debo poner muchos_peces[3]. 
  */
 
  /**
-  * Ejecuta el siguiente código e intenta averiguar que está sucediendo. Porque el úñtimo mensaje dice "Pez siguiente: undefined?"
+  * Ejecuta el siguiente código e intenta averiguar que está sucediendo. CONTESTA A LA PREGUNTA: ¿Porque el úñtimo mensaje dice "Pez siguiente: undefined?"
   */
 
 let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
