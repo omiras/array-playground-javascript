bb82b2d3e7d06b86e0ea89956302492ddd615e0c
Oscar Miras <oscar.miras@gmail.com>
2022-01-06 15:43:48 +0100

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -11,7 +11,7 @@
  console.log("Nemo está en la posición: " + pezPosicion)
 
  /**
-  * Fíjate sin embargo qué valor arroja aplicar indexOf cuando buscamos a "estrella". ¿Por qué crees que sucede esto?
+  * Fíjate sin embargo qué valor arroja aplicar indexOf cuando buscamos a "estrella". CONTESTA: ¿Por qué crees que sucede esto?
   */
 
   let pezPosicion2 = muchos_peces.indexOf("estrella")
