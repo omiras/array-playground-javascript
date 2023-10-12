c8a2d4d9c35d0471f2f70a0b79703480cd40dac5
omiras <oscar.miras@gmail.com>
2023-10-12 09:00:02 +0200

    camelCase

--- ex2.js
+++ ex2.js
@@ -1,21 +1,18 @@
 /**
  * Recorrer arrays
- * 
- * Una actividad muy común en programación es recorrer los arrays en busca de un elemento
- * 
- * Añade un condicional if... que compruebe si, la variable "siguiente_pez" es "Nemo". Si es "Nemo" muestra por el terminal un mensaje: "He encontrado a Nemo!". Cada vez que no encuentre el pez Nemo, escribe "Este pez NO es Nemo!"
  *
- * Tu código debería escribir un total de 4 mensajes. 
+ * Una actividad muy común en programación es recorrer los arrays en busca de un elemento
+ *
+ * Añade un condicional if... que compruebe si, la variable "siguentePez" es "Nemo". Si es "Nemo" muestra por el terminal un mensaje: "He encontrado a Nemo!". En caso contrario, debe escribir "Este pez NO es Nemo!";
+ *
+ * Tu código debería escribir un total de 4 mensajes.
  */
 
-let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
+let muchosPeces = ["Dory", "Nemo", "Shipho", "Estrella"];
 
-for(let i=0; i<muchos_peces.length; i++) {
-    let siguiente_pez = muchos_peces[i]
-    console.log("Pez siguiente: " + siguiente_pez)
+for (let i = 0; i < muchosPeces.length; i++) {
+  let siguientePez = muchosPeces[i];
+  console.log("Pez siguiente: " + siguientePez);
 
-    // Modifica SOLAMENTE a partir de aquí
+  // Modifica SOLAMENTE a partir de aquí
 }
-
-
-
