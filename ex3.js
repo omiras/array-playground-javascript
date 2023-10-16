caeb1e9fd34723b3b4fe26d0c75172e7ad03e549
omiras <oscar.miras@gmail.com>
2023-10-16 08:44:07 +0200

    info

--- ex3.js
+++ ex3.js
@@ -1,27 +1,30 @@
 /**
  * Los límites del array
- * 
+ *
  * Una de las cosas que más cuesta entender al comenzar programación, es que la primera posición del array, viene dada en realidad por la posición "cero".
- * 
+ *
  * A la posición que ocupa un elemento en un array se le llama "índice"
- * 
+ *
  * Así, lista_peces[0] nos da el primer pez
  * lista_peces[1] nos da el segundo pez
  * ..y así hasta el último.
- * 
+ *
  * En el array: let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
- * 
- * El array tiene 4 elementos. Para acceder a "Dory" debo poner muchos_peces[0]. Para acceder a "Estrella" debo poner muchos_peces[3]. 
+ *
+ * El array tiene 4 elementos. Para acceder a "Dory" debo poner muchos_peces[0]. Para acceder a "Estrella" debo poner muchos_peces[3].
  */
 
- /**
-  * Ejecuta el siguiente código e intenta averiguar que está sucediendo. CONTESTA A LA PREGUNTA: ¿Porque el úñtimo mensaje dice "Pez siguiente: undefined?"
-  */
+/**
+ * Ejecuta el siguiente código e intenta averiguar que está sucediendo. CONTESTA A LA PREGUNTA: ¿Porque el úñtimo mensaje dice "Pez siguiente: undefined?"
+ */
 
-let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
+let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"];
 
-for(let i=0; i<=muchos_peces.length; i++) {
-    let siguiente_pez = muchos_peces[i]
-    console.log("Pez siguiente: " + siguiente_pez)
+for (let i = 0; i <= muchos_peces.length; i++) {
+  let siguiente_pez = muchos_peces[i];
+  console.log("Pez siguiente: " + siguiente_pez);
 }
 
+/**
+ * BONUS: Este ejercicio no sigue la convención de JavaScript camelCase. ¿Podrías modificar el nombre de todas las variables para adecuarlas a la norma?
+ */
--- ex6.js
+++ ex6.js
@@ -3,29 +3,29 @@
  */
 
 function existePez(listaPeces, pezBuscado) {
-    let pezEncontrado = false
-    let i = 0
+  let pezEncontrado = false;
+  let i = 0;
 
-    while(!pezEncontrado && i<listaPeces.length-1) {
-        pezEncontrado = (listaPeces[i] == pezBuscado) 
-        i++      
-    }
+  while (!pezEncontrado && i < listaPeces.length - 1) {
+    pezEncontrado = listaPeces[i] == pezBuscado;
+    i++;
+  }
 
-    return pezEncontrado
+  return pezEncontrado;
 }
 
-let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
-let he_encontrado_al_pez = existePez(muchos_peces, "Estrella")
+let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"];
+let he_encontrado_al_pez = existePez(muchos_peces, "Estrella");
 
-console.log("He encontrado al pez?: " + he_encontrado_al_pez)
+console.log("He encontrado al pez?: " + he_encontrado_al_pez);
 
 /**
  * Ejecuta el código.
  * Esta functión debería devolver "true"; puesto que "Estrella"; efectivamente está en el array.
- * Sin embargo devuelve false. Hay dos errores:
- * 
+ * Sin embargo devuelve false. Hay DOS errores:
+ *
  * 1. Un error que impide ejecutar el código. Vigila la información que arroja el TERMINAL
  * 2. Un error lógico; que hace que el código no devuelve lo que debería devolver.
- * 
+ *
  * Pon "chivatos" en el código para aydarte a averiguar que está sucediendo; o usa el depurador de Visual Studio para ejecutar el código paso a paso
  */
