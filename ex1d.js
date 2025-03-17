a0820bd007cfa0748b2097fffeeda159c650608f
omiras <oscar.miras@gmail.com>
2025-03-17 17:26:45 +0100

    done ex1d

--- ex1d.js
+++ ex1d.js
@@ -8,4 +8,4 @@ cola.push("Carlos");
 
 console.log("Cola del supermercado: ", cola);
 
-// En este punto la cajera o el cajero van a llamar a la siguiente persona. Obviamente le toca a "Juan" que era el primero que iba en la cola. ¿Qué método JavaScript podemos utilizar para eliminar el primer elemento de este array? Muestra tus resultados por consol.log
+// En este punto la cajera o el cajero van a llamar a la siguiente persona. Obviamente le toca a "Juan" que era el primero que iba en la cola. ¿Qué método JavaScript podemos utilizar para eliminar el primer elemento de este array? Muestra tus resultados por consol.loge . Resultado esperado: https://oscarm.tinytake.com/media/173dbdd?filename=1742228755529_TinyTake17-03-2025-05-25-47_638778255552975975.png&sub_type=thumbnail_preview&type=attachment&width=798&height=110
