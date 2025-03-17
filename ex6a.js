9aa9bd5ccbcdaa75ac43bf45aa65f19d001e1c3d
omiras <oscar.miras@gmail.com>
2025-03-17 17:20:34 +0100

    done

---
+++ ex6a.js
@@ -0,0 +1,22 @@
+/**
+ * Convertir los nombres de los jugadores a mayúsculas
+ * 
+ * En Mario Party, cuando un jugador gana una estrella, grita su nombre con emoción.
+ * Dado un array de nombres de jugadores, devuelve un nuevo array donde todos los nombres están en mayúsculas.
+ */
+
+function shoutPlayerNames(players) {
+    // RECUERDA: PRIMERO escribe lo que hay que hacer con tus palabras. Luego, viene el código
+    let arrayUpperCase = [];
+
+    // Codificar aquí....
+
+    return arrayUpperCase;
+
+}
+
+// Ejemplo de uso:
+console.log(shoutPlayerNames(["Mario", "Luigi", "Peach"])); // ["MARIO", "LUIGI", "PEACH"]
+console.log(shoutPlayerNames(["Bowser", "Yoshi", "Toad"])); // ["BOWSER", "YOSHI", "TOAD"]
+console.log(shoutPlayerNames(["daisy", "wario", "waluigi"])); // ["DAISY", "WARIO", "WALUIGI"]
+console.log(shoutPlayerNames(["rosalina"])); // ["ROSALINA"] -> Un solo jugador también grita
