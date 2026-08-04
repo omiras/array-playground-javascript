4b37cda4e11050edb6a087da41e91dfc32189c36
omiras <oscar.miras@gmail.com>
2026-08-04 12:14:30 +0200

    done

--- ex1c.js
+++ ex1c.js
@@ -5,7 +5,7 @@
 let reyesMagos = ["Melchor", "Baltasar", "Gaspar"];
 
 /**
- * Modifica el array a partír de sus índices para obtener el orden adecuado de los reyes magos. Mira esta ayuda: https://chatgpt.com/c/6a7186a3-20a8-83eb-8f27-86d86ab86c98
+ * Modifica el array a partír de sus índices para obtener el orden adecuado de los reyes magos. Mira esta ayuda: https://share.gemini.google/7y8uqAQxXftW
  */
 
 // Tú código de partir de aquí
