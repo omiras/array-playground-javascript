84097149b3d34abd7860c351e89cbff447046f2c
omiras <oscar.miras@gmail.com>
2026-08-04 08:49:58 +0200

    done

---
+++ ex1e.js
@@ -0,0 +1,13 @@
+/**
+ * Concatenar arrays
+ */
+
+const array1 = [3, 4, 5];
+const array2 = [1, 2, 3];
+
+/**
+ * Modifica la asignación para concatenar los dos array según el orden natural de los númerps [1,2,3,4,5,6]; Clic para un ejemplo: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+ */
+
+let arrayResultado = array2.concat(array1); // <--- MODIFICAR
+console.log(arrayResultado);
