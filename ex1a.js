d1a81f43d02550fb6d7578c78324ef1543e3daed
omiras <oscar.miras@gmail.com>
2026-08-04 08:45:01 +0200

    done

---
+++ ex1a.js
@@ -0,0 +1,9 @@
+/**
+ * Crea un array que contenga 3 elementos
+ * 1. El primer elemento es tu nombre
+ * 2. El segundo elemento es tu edad
+ * 3. El tercer elemento es un boleano donde true significa que estás trabajando, y false que no estás trabajando en este momento
+ * 4. Muestra el array por el terminal
+ */
+let informacionPersonal = ["Oscar", 44, true];
+console.log(informacionPersonal);
