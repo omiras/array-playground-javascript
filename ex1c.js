b8723570137e8e486053cae1ebf5b5b84b2aec29
omiras <oscar.miras@gmail.com>
2026-08-04 08:31:17 +0200

    done

--- ex1c.js
+++ ex1c.js
@@ -2,7 +2,7 @@
  * Dado el array de reyes magos
  */
 
-let peces = ["Melchor", "Baltasar", "Gaspar"];
+let reyesMagos = ["Melchor", "Baltasar", "Gaspar"];
 
 /**
  * Modifica el array a partír de sus índices para obtener el orden adecuado de los reyes magos. Mira esta ayuda: https://chatgpt.com/c/6a7186a3-20a8-83eb-8f27-86d86ab86c98
@@ -10,4 +10,4 @@ let peces = ["Melchor", "Baltasar", "Gaspar"];
 
 // Tú código de partir de aquí
 
-console.log(peces); // ["Melchor", "Gaspar", "Baltasar"]
+console.log(reyesMagos); // ["Melchor", "Gaspar", "Baltasar"]
